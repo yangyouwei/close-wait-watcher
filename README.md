607321fab812a4f3a1e7d3da3289d360680237d8
dabai <yang_youwei81@163.com>
2019-09-05 11:38:58 +0800

    first

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # close-wait-watcher
+
+//执行系统命令
+f, err := exec.LookPath("ls")  
+if err != nil {  
+    fmt.Println(err)  
+}  
+fmt.Println(f) //  /bin/ls  
+
+
+netstat -n | awk '/^tcp/ {++S[$NF]} END {for(a in S) print a, S[a]}'
+
+//定时器
+
+
+func main() {
+    ticker := time.NewTicker(5 * time.Second)
+    quit := make(chan int)
+    var wg  sync.WaitGroup
+ 
+    wg.Add(1)
+    go func() {
+        defer wg.Done()
+        fmt.Println("child goroutine bootstrap start")
+        for {
+            select {
+                case <- ticker.C:
+                    fmt.Println("ticker .")
+                case <- quit:
+                    fmt.Println("work well .")
+                    ticker.Stop()
+                    return
+            }
+        }
+        fmt.Println("child goroutine bootstrap end")
+    }()
+    time.Sleep(10 * time.Second)
+    quit <- 1
+    wg.Wait()
+}
+
+//CLOSE_WAIT
+
+conf
+
+//secod
+interval=5
+//colose_wait number
+close_wait_max=5000
+//restart service
+service_command="service restart ss"
---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/Unknwon/goconfig"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+
+const ShellToUse = "bash"
+
+type confstr struct {
+	interval int
+	close_wait_max int
+	service_command string
+	watch_command string
+}
+
+var conf_vale confstr
+
+func init() {
+	cfg, err := goconfig.LoadConfigFile("conf")
+	if err != nil {
+		log.Println("读取配置文件失败[config.ini]")
+		panic(err)
+	}
+	conf_vale.Getconf(cfg,err)
+}
+
+func (this *confstr) Getconf(c *goconfig.ConfigFile,err error) {
+	this.service_command, err  = c.GetValue("main","service_command")
+	if err != nil {
+		log.Panic(err)
+	}
+	this.watch_command = "netstat -n | awk '/^tcp/ {++S[$NF]} END {for(a in S) print a, S[a]}'"
+
+	close_num ,err:= c.GetValue("main","close_wait_max")
+	if err != nil {
+		log.Panic(err)
+	}
+	this.close_wait_max,err = strconv.Atoi(close_num)
+	if err != nil {
+		log.Panic(err)
+	}
+	interval_num, err := c.GetValue("main","interval")
+	if err != nil {
+		log.Panic(err)
+	}
+	this.interval, err = strconv.Atoi(interval_num)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func Shellout(command string) (error, string, string) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command(ShellToUse, "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return err, stdout.String(), stderr.String()
+}
+
+
+
+func get_close_wait_num(c string) (num int){
+	err, sout ,_ := Shellout(c)
+	if err != nil {
+		log.Println(err)
+	}
+	a := strings.Split(sout,"\n")
+	for _,i := range a {
+		res := strings.HasPrefix(i,"CLOSE_WAIT")
+		if res {
+				fmt.Println(i)
+				snum := strings.Split(i," ")
+				num,err = strconv.Atoi(fmt.Sprint(snum[1]))
+				break
+		}
+	}
+	return num
+}
+
+func restart_service(c string)  {
+	err, sout ,serr := Shellout(c)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(sout)
+	log.Println(serr)
+}
+
+func main()  {
+	ticker := time.NewTicker(5 * time.Second)
+	var wg  sync.WaitGroup
+
+	wg.Add(1)
+	log.Println("service wather start")
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <- ticker.C:
+				num := get_close_wait_num(conf_vale.watch_command)
+				fmt.Println(num)
+				if num > conf_vale.close_wait_max {
+					restart_service(conf_vale.service_command)
+				}
+			}
+		}
+	}()
+	wg.Wait()
+}
